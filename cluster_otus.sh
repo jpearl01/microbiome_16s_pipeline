88034c51f0a7b592bb96146b4caff48426a8128c
Josh Earl <joshearl1@hotmail.com>
2014-12-16 15:35:54 -0500

    Modified pipeline to actually do custering

--- cluster_otus.sh
+++ cluster_otus.sh
@@ -19,20 +19,23 @@ derep_fasta=${samp_name}_ee${ee}_tunc${trunc_len}_derep.fasta
 echo -e "Derep output:"'\t'$derep_fasta
 sorted=${samp_name}_ee${ee}_tunc${trunc_len}_sorted.fasta
 otus=${samp_name}_ee${ee}_tunc${trunc_len}_otus.fasta
+named=${samp_name}_ee${ee}_tunc${trunc_len}_otus_named.fasta
 readmap=${samp_name}_ee${ee}_tunc${trunc_len}.uc
 table=${samp_name}_ee${ee}_tunc${trunc_len}.table
 utax=${samp_name}_ee${ee}_tunc${trunc_len}_utax.results
 to_plot="data_to_plot.dat"
 
 usearch -fastq_filter $fastq_file  -fastaout $fasta_reads -fastq_maxee $ee -fastq_trunclen $trunc_len -relabel barcodelabel=${samp_name}\;${samp_name}_ -eeout
-usearch -derep_fulllength $fasta_reads -output $derep_fasta -sizeout -minseqlength 300
-usearch -sortbysize $derep_fasta -output $sorted -minsize 4
-python ~/programs/usearch/fasta_number.py $sorted OTU_ > $otus
-usearch -usearch_global $fasta_reads -db $otus -strand plus -id 0.97 -uc $readmap
+usearch -derep_fulllength $fasta_reads -output $derep_fasta -sizeout -minseqlength 400
+usearch -sortbysize $derep_fasta -output $sorted -minsize 2
+
+usearch -cluster_otus $sorted -otus $otus
+python ~/programs/usearch/fasta_number.py $otus OTU_ > $named
+usearch -usearch_global $fasta_reads -db $named -strand plus -id 0.97 -uc $readmap
 python ~/programs/usearch/uc2otutab.py $readmap > $table
-usearch8 -utax $otus -db ~/Documents/rdp_16s.fa -strand plus -utax_rawscore -tt ~/Documents/rdp_16s.tt -utaxout $utax
+usearch8 -utax $named -db ~/Documents/rdp_16s.fa -strand plus -utax_rawscore -tt ~/Documents/rdp_16s.tt -utaxout $utax
 #create a sorted file for the plot function
 #grep 'OTUId' $table > ${to_plot}
 #grep -v 'OTUId' $table | sort -n -k2 >> ${to_plot}
-/home/josh/projects/brain/sort_and_filter_table.rb $utax $table
-gnuplot -e "filename='${to_plot}'" /home/josh/projects/brain/plot.sh
+/home/josh/workspace/ruby/microbiome_16s_pipeline/sort_and_filter_table.rb $utax $table
+gnuplot -e "filename='${to_plot}'" /home/josh/workspace/ruby/microbiome_16s_pipeline/plot.sh
