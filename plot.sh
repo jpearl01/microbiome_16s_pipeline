0f785a4ce6b63b005ce94b115fbb97fe7de3d81f
Josh Earl <joshearl1@hotmail.com>
2015-01-22 14:59:16 -0500

    Can't remember how i modified it, but plot.sh changed

--- plot.sh
+++ plot.sh
@@ -5,9 +5,9 @@ set term png
 set output "box_plot.png"
 set xlabel "OTUs"
 set ylabel "Number of OTUs"
-set xtics rotate
+set xtics rotate by 45 right
 #set xtics font ", 3" 
-set terminal png size 2400,1600
+set terminal png size 2500,1600
 plot COL=2 filename using COL:xticlabels(1)
 replot
 #pause -1
