7725acecdaed3143651358af336d1fe8173a2beb
Josh Earl <joshearl1@hotmail.com>
2015-04-07 15:45:42 -0400

    optimized code a bit, also added in the ability to read the master log, so we can get a date/time

--- get_raw_read_counts.rb
+++ get_raw_read_counts.rb
@@ -6,7 +6,7 @@
 require 'json'
 require 'yaml'
 
-abort("Can't open the sample pool file!") unless File.exists?("sample_key_2.txt")
+abort("Can't open the sample (key) pool file!") unless File.exists?("sample_key_2.txt")
 
 
 class Barcode_16s_record
@@ -39,56 +39,53 @@ log = File.open('log_barcodes.txt', 'w')
 pb_projects.each do |id, samps|
 
 	curr_file = "/data/pacbio/smrtanalysis_userdata/jobs/016/0" + id + "/results/barcode_report.json"
+	m_log = "/data/pacbio/smrtanalysis_userdata/jobs/016/0" + id + "/log/master.log"
+	abort("Can't open the json file: #{curr_file}") unless File.exists?(curr_file)	
+	abort("Can't open the master log file: #{m_log}") unless File.exists?(m_log)
+
 	log.puts("The current file is: " + curr_file)
 
-	puts curr_file
-	if File.exists?(curr_file)
-		curr_file = File.read(curr_file)
-		barcodes = JSON.parse(curr_file)
 
+	barcodes = JSON.parse(File.read(curr_file))
+	samps.each do |rec|
 
-		samps.each do |rec|
-
-			if rec.barcode_num.to_i == 1
-				barcode_name = '0001_Forward--0002_Forward'
-				if barcodes["tables"][0]['columns'][0]['values'].index(barcode_name).nil?
-					reads = 0
-				else
-					ind = barcodes["tables"][0]['columns'][0]['values'].index(barcode_name)
-					reads = barcodes["tables"][0]['columns'][1]['values'][ind]
-				end
-				puts "#{id}\t#{rec.site_id}\t#{rec.patient}\t#{barcode_name}\t#{reads}"
-			elsif rec.barcode_num.to_i == 2
-				barcode_name = '0003_Forward--0004_Forward'
-				if barcodes["tables"][0]['columns'][0]['values'].index(barcode_name).nil?
-					reads = 0
-				else
-					ind = barcodes["tables"][0]['columns'][0]['values'].index(barcode_name)
-					reads = barcodes["tables"][0]['columns'][1]['values'][ind]
-				end
-				puts "#{id}\t#{rec.site_id}\t#{rec.patient}\t#{barcode_name}\t#{reads}"
-			elsif rec.barcode_num.to_i == 3
-				barcode_name = '0005_Forward--0006_Forward'
-
-				if barcodes["tables"][0]['columns'][0]['values'].index(barcode_name).nil?
-					reads = 0
-				else
-					ind = barcodes["tables"][0]['columns'][0]['values'].index(barcode_name)
-					reads = barcodes["tables"][0]['columns'][1]['values'][ind]
-				end
-				puts "#{id}\t#{rec.site_id}\t#{rec.patient}\t#{barcode_name}\t#{reads}"
-			elsif rec.barcode_num.to_i == 4
-				barcode_name = '0007_Forward--0008_Forward'
-				if barcodes["tables"][0]['columns'][0]['values'].index(barcode_name).nil?
-					reads = 0
-				else
-					ind = barcodes["tables"][0]['columns'][0]['values'].index(barcode_name)
-					reads = barcodes["tables"][0]['columns'][1]['values'][ind]
-				end
-				puts "#{id}\t#{rec.site_id}\t#{rec.patient}\t#{barcode_name}\t#{reads}"
+		if rec.barcode_num.to_i == 1
+			barcode_name = '0001_Forward--0002_Forward'
+			if barcodes["tables"][0]['columns'][0]['values'].index(barcode_name).nil?
+				reads = 0
+			else
+				ind = barcodes["tables"][0]['columns'][0]['values'].index(barcode_name)
+				reads = barcodes["tables"][0]['columns'][1]['values'][ind]
 			end
+			puts "#{id}\t#{rec.site_id}\t#{rec.patient}\t#{barcode_name}\t#{reads}"
+		elsif rec.barcode_num.to_i == 2
+			barcode_name = '0003_Forward--0004_Forward'
+			if barcodes["tables"][0]['columns'][0]['values'].index(barcode_name).nil?
+				reads = 0
+			else
+				ind = barcodes["tables"][0]['columns'][0]['values'].index(barcode_name)
+				reads = barcodes["tables"][0]['columns'][1]['values'][ind]
+			end
+			puts "#{id}\t#{rec.site_id}\t#{rec.patient}\t#{barcode_name}\t#{reads}"
+		elsif rec.barcode_num.to_i == 3
+			barcode_name = '0005_Forward--0006_Forward'
+
+			if barcodes["tables"][0]['columns'][0]['values'].index(barcode_name).nil?
+				reads = 0
+			else
+				ind = barcodes["tables"][0]['columns'][0]['values'].index(barcode_name)
+				reads = barcodes["tables"][0]['columns'][1]['values'][ind]
+			end
+			puts "#{id}\t#{rec.site_id}\t#{rec.patient}\t#{barcode_name}\t#{reads}"
+		elsif rec.barcode_num.to_i == 4
+			barcode_name = '0007_Forward--0008_Forward'
+			if barcodes["tables"][0]['columns'][0]['values'].index(barcode_name).nil?
+				reads = 0
+			else
+				ind = barcodes["tables"][0]['columns'][0]['values'].index(barcode_name)
+				reads = barcodes["tables"][0]['columns'][1]['values'][ind]
+			end
+			puts "#{id}\t#{rec.site_id}\t#{rec.patient}\t#{barcode_name}\t#{reads}"
 		end
-	else
-		abort("Yo, the file: #{curr_file} doesn't exist")
 	end
-	end
+end
