28e42edad28a18eb831627dc67f7c24df643100d
Josh Earl <joshearl1@hotmail.com>
2015-04-08 13:04:51 -0400

    added in the reverse complement ability in the nasal_trim_primers program

--- nasal_trim_primers.rb
+++ nasal_trim_primers.rb
@@ -35,32 +35,61 @@ Bio::FlatFile.auto(ARGV[0]) do |ff|
   end
   ff.each do |entry|
     seq = entry.naseq
+    is_comp = false
     total_seqs += 1
     f_match = f_re.match(seq)
     r_match = r_re.match(seq)
     forward_match += 1 if !f_match.nil? 
     reverse_match += 1 if !r_match.nil? 
+    #Adding in the ability to check the reverse complement of the sequence, if there was no hit
+    if f_match.nil? && r_match.nil?
+        f_match = f_re.match(seq.complement)
+        r_match = r_re.match(seq.complement)
+        forward_match += 1 if !f_match.nil? 
+        reverse_match += 1 if !r_match.nil? 
+        is_comp = true
+    end
+
     if db_class == 'fasta'
       if !r_match.nil? && !f_match.nil?
         both_match += 1 
-        abort("#{r_match}") if r_match[1].nil?
-        sub_seq = seq.subseq(f_match[1].length + 1, seq.length - r_match[1].length)
-        cap_sub_seq = sub_seq.swapcase
-        output.puts(">#{entry.definition}")
-        output.puts(cap_sub_seq)
-      end
+        abort("Unexpected match: #{r_match}") if r_match[1].nil?
+        if is_comp
+            seq = seq.complement
+            sub_seq = seq.subseq(f_match[1].length + 1, seq.length - r_match[1].length)
+            cap_sub_seq = sub_seq.swapcase
+            output.puts(">#{entry.definition}")
+            output.puts(cap_sub_seq)
+        else
+            sub_seq = seq.subseq(f_match[1].length + 1, seq.length - r_match[1].length)
+            cap_sub_seq = sub_seq.swapcase
+            output.puts(">#{entry.definition}")
+            output.puts(cap_sub_seq)
+        end
+    end
     else
       if !r_match.nil? && !f_match.nil?
         both_match += 1
-
-        sub_seq = seq.subseq(f_match[1].length + 1, seq.length - r_match[1].length)
-        cap_sub_seq = sub_seq.swapcase
-        end_qual = seq.length - r_match[1].length - 1
-        sub_qual = entry.quality_string[f_match[1].length .. end_qual ]
-        output.write('@' + entry.definition + "\n")
-        output.write(cap_sub_seq)
-        output.write("\n+\n")
-        output.write(sub_qual + "\n")
+        if is_comp
+            seq = seq.complement
+            sub_seq = seq.subseq(f_match[1].length + 1, seq.length - r_match[1].length)
+            cap_sub_seq = sub_seq.swapcase
+            end_qual = seq.length - r_match[1].length - 1
+            sub_qual = entry.quality_string.reverse[f_match[1].length .. end_qual ]
+            output.write('@' + entry.definition + "\n")
+            output.write(cap_sub_seq)
+            output.write("\n+\n")
+            output.write(sub_qual + "\n")
+        else
+            sub_seq = seq.subseq(f_match[1].length + 1, seq.length - r_match[1].length)
+            cap_sub_seq = sub_seq.swapcase
+            end_qual = seq.length - r_match[1].length - 1
+            sub_qual = entry.quality_string[f_match[1].length .. end_qual ]
+            output.write('@' + entry.definition + "\n")
+            output.write(cap_sub_seq)
+            output.write("\n+\n")
+            output.write(sub_qual + "\n")
+        end
       end
     end
 
