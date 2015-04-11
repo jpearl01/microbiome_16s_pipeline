36e61105fbe8b1217aa785f275482e78e68e0d79
Josh Earl <joshearl1@hotmail.com>
2015-04-10 20:49:24 -0400

    changed it so that we were now using the updated 64bit database

--- run_pipe_nasal.sh
+++ run_pipe_nasal.sh
@@ -42,9 +42,9 @@ usearch -derep_fulllength $fastq_file -fastaout derep.fasta -sizeout
 usearch -sortbysize derep.fasta -fastaout sorted.fasta
 usearch -cluster_otus sorted.fasta -otus old_otus.fasta -uparseout uparseout
 fasta_number.py old_otus.fasta OTU_ > otus.fasta
-usearch -usearch_global $fastq_file -db otus.fasta -strand both -id 0.97 -uc readmap
+usearch -usearch_global $fastq_file -db otus.fasta -strand both -uc readmap
 python ~/bin/uc2otutab.py readmap > table
 #Remember to change the taxconfs if not using fl (full length) sequences
-usearch -utax otus.fasta -db ~/Documents/rdp_16s_8.udb -taxconfs ~/Documents/rdp_16s_fl.tc -tt ~/Documents/rdp_16s.tt -utaxout utax
+usearch -utax otus.fasta -db ~/Documents/rdp_16s_64bit.udb -taxconfs ~/Documents/rdp_16s_fl.tc -tt ~/Documents/rdp_16s.tt -utaxout utax
 sort_and_filter_table.rb utax table
 gnuplot -e "filename='data_to_plot.dat'" ~/bin/plot.sh
